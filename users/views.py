9f1ed4b63285c3c832ef579ee098c793e27384ae
evigne <vigneshwaranethirajan@gmail.com>
2018-10-30 17:40:34 +0530

    add user login

--- users/views.py
+++ users/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from users.forms import UserRegisterForm
 
 def register(request):
     if request.method =='POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
-            messages.success(request,f'Account Created for {username}')
-            return redirect('blog-home')
+            messages.success(request,f'Welcome to JustThoughts {username}, you can now login')
+            return redirect('users_login')
 
 
 
     elif request.method == 'GET':
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
 
 # Create your views here.
