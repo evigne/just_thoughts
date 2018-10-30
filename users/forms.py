56d2bd229ffdcefd43aa3afc55073cde75c5dd71
evigne <vigneshwaranethirajan@gmail.com>
2018-10-30 23:10:35 +0530

    add userprofile update,profile image resize

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from . models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -10,3 +11,18 @@ class UserRegisterForm(UserCreationForm):
     class Meta():
         model = User
         fields = ['username','first_name','last_name','email','password1','password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
+    class Meta():
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta():
+        model = Profile
+        fields = ['image']
--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User  # extending the default User model by django
+from PIL import Image  #to resize the profile image
 
-class Profile(models.Model):
+class Profile(models.Model): # extending the default User model by django
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg',upload_to='profile_pics')
     #settings.py MEDIA_ROOT = ''
@@ -9,3 +10,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    def save(self): # already exists and we alter to resize the profile pictures
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height>300 or img.width>300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
