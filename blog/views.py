8e065ab5be348ce9aa845b0ecc29a0b7cb4239cf
evigne <vigneshwaranethirajan@gmail.com>
2018-11-07 20:59:26 +0530

    add new post - submit

--- blog/views.py
+++ blog/views.py
@@ -18,24 +18,33 @@ def home(request):
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'  #<app>/<model>_<view_type>.html
+    template_name = 'blog/home.html'  # <app>/<model>_<view_type>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
 
 class PostDetailView(DetailView):
     model = Post
-    # template_name = 'blog/post_detail.html' "<app>/<model>_<view_type>.html"  not required since its the default path and name
+
+
+""" 
+    template_name = 'blog/post_detail.html' "<app>/<model>_<view_type>.html"  
+    not required since its the default path and name
     #By Default
     # context_object_name = 'object'
     # thats why change post to object
+"""
 
 
 class PostCreateView(CreateView):
     model = Post
     fields = ['title','content']
-    #template_name = "<model_name>_form.html"
+    # template_name = "<model_name>_form.html" default
+    #success_url = 'blog-home'
 
+    def form_valid(self, form): # tells that the form belong to the user logged in
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
