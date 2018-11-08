807d155491d95ee85eb1249c3a289c979a5d269b
evigne <vigneshwaranethirajan@gmail.com>
2018-11-08 18:09:42 +0530

    complete post create, update, delete

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+# LoginRequiredMixin for login requrired and UserPassesTestMixin for make sure the user is one does the update to post
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView,
+    DeleteView
 )
 
 posts = Post.objects.all()
@@ -36,7 +40,7 @@ class PostDetailView(DetailView):
 """
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
     fields = ['title','content']
     # template_name = "<model_name>_form.html" default
@@ -45,6 +49,33 @@ class PostCreateView(CreateView):
     def form_valid(self, form): # tells that the form belong to the user logged in
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+    # will use same template as create post 'post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
