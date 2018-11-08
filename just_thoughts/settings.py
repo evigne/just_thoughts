29c06736bc657275f4b4ed870bc1a03ebc2bb673
evigne <vigneshwaranethirajan@gmail.com>
2018-11-09 00:33:59 +0530

    add password config

--- just_thoughts/settings.py
+++ just_thoughts/settings.py
@@ -132,3 +132,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'users_login'
+
+# SetUP EMAIL
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER ="justthoughtshelp@gmail.com"
+EMAIL_HOST_PASSWORD ="wnhchhabbttwemrn"
+
