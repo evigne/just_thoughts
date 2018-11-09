053a8354a1d9df7cefb7224bee60efc96c406dd5
evigne <vigneshwaranethirajan@gmail.com>
2018-11-09 18:48:14 +0530

    project phase1 completed and updated in the future

--- just_thoughts/settings.py
+++ just_thoughts/settings.py
@@ -139,6 +139,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ="justthoughtshelp@gmail.com"
-EMAIL_HOST_PASSWORD ="wnhchhabbttwemrn"
-
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+ACCOUNT_EMAIL_VERIFICATION = 'none'
